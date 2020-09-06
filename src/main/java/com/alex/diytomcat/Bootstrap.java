8f32c490fc37cff9d03d42a335f86a7a204672e3
Alex Chen <alexchen@Alexs-iMac.attlocal.net>
2020-09-05 21:33:13 -0700

    [Init] initialize project

---
+++ src/main/java/com/alex/diytomcat/Bootstrap.java
@@ -0,0 +1,46 @@
+package com.alex.diytomcat;
+
+import cn.hutool.core.util.NetUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+
+        try {
+            int port = 18080;
+
+            if (!NetUtil.isUsableLocalPort(port)) {
+                System.out.println(port + " is in use");
+                return;
+            }
+            ServerSocket ss = new ServerSocket(port);
+
+            while (true) {
+                Socket s = ss.accept();
+                InputStream is = s.getInputStream();
+                int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+                is.read(buffer);
+                String requestString = new String(buffer, "utf-8");
+                System.out.println("Request is \r\n" + requestString);
+
+                OutputStream os = s.getOutputStream();
+                String response_head = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n\r\n";
+                String responseString = "Hello From Alex's DIY Tomcat";
+                responseString = response_head + responseString;
+                os.write(responseString.getBytes());
+                os.flush();
+                s.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
